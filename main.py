7b945e7f990e16ab587d695c964a4332e11ec8c3
ayushmancpanigrahi <payushman600@gmail.com>
2025-07-02 18:25:53 +0530

    First Commit of this project

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import numpy as np
 import joblib
+import os
 
 app = Flask(__name__)
 
@@ -43,4 +44,5 @@ def predict():
         return render_template('index.html', error=str(e))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
